d49ee9756b4c2866830b0c9ab2e5f44ea6447517
shenruipan <shenrui@uchicago.edu>
2016-10-31 16:27:53 -0500

    Code for python
    python is not boring

--- gibbsampling.py
+++ gibbsampling.py
@@ -1,14 +1,29 @@
-import random,math
+from math import *
+import random
+import numpy
+import matplotlib.pyplot as plt
 B = 5
-def gibbs(T=500):
-    x = 1
-    y = 1
+T = 50000
+M = numpy.zeros((T, 3))
+
+
+def gibbs(T,thin=10):
+    x=1
+    y=1
     print "Iter  x  y"
     for i in range(T):
-        if 0 < x < B:
-            if 0 < y < B:
-                x=random.expovariate(y)
-                y=random.expovariate(x)
-                print i,x,y
+        for j in range(thin):
+            p1= random.uniform(0,1)
+            x=-1/y*log(1-p1+p1*exp(-y*B))
+            p2= random.uniform(0,1)
+            y=-1/x*log(1-p2+p2*exp(-x*B))
+            M[i,0] = i
+            M[i,1] = x
+            M[i,2] = y
+        #print i,x,y
 
-gibbs()
+gibbs(T)
+#print M
+print numpy.mean(M[:,1])
+print numpy.mean(M[:,2])
+plt.hist(M[:,1])
